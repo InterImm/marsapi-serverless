c60bd7fb96ed7e795683ef1b39049ce4cd2e5e44
OctoMiao <emptymalei@qq.com>
2019-07-01 22:47:08 +0200

    using our own leapseconds file

---
+++ functions/leapseconds.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+__cwd__ = os.getcwd()
+__location__ = os.path.realpath(
+    os.path.join(__cwd__, os.path.dirname(__file__))
+    )
+
+_LEAPSECONDS_JSON_PATH = os.path.join(
+    __location__, 'leapseconds/leapseconds.json'
+    )
+
+def get_leapseconds():
+    """Load leapseconds from json file
+    """
+
+    with open(_LEAPSECONDS_JSON_PATH, 'r') as fp:
+        leapseconds_list = json.load(fp).get('data')
+
+    leapseconds = []
+    for i in leapseconds_list:
+        leapseconds.append(
+            (
+                int(float(
+                    i.get('epoch')
+                    )),
+                    int(
+                        float(
+                            i.get('seconds')
+                        )
+                        )
+                )
+            )
+
+    return tuple(leapseconds)
