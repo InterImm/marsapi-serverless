7e2a6bd38f3f34337b428f21336430305eebb09e
OctoMiao <emptymalei@qq.com>
2019-07-01 22:46:54 +0200

    using our own leapseconds file

--- functions/mars.py
+++ functions/mars.py
@@ -1,19 +1,8 @@
 import time
 import sys
+from functions.leapseconds import get_leapseconds as _get_leapseconds
 
-# From <https://www.ietf.org/timezones/data/leap-seconds.list>
-# Needs to be updated on: 28 December 2018 (3754944000)
-
-LEAP_SECONDS = (
-    (2272060800, 10), (2287785600, 11), (2303683200, 12), (2335219200, 13),
-    (2366755200, 14), (2398291200, 15), (2429913600, 16), (2461449600, 17),
-    (2492985600, 18), (2524521600, 19), (2571782400, 20), (2603318400, 21),
-    (2634854400, 22), (2698012800, 23), (2776982400, 24), (2840140800, 25),
-    (2871676800, 26), (2918937600, 27), (2950473600, 28), (2982009600, 29),
-    (3029443200, 30), (3076704000, 31), (3124137600, 32), (3345062400, 33),
-    (3439756800, 34), (3550089600, 35), (3644697600, 36), (3692217600, 37)
-)
-
+LEAP_SECONDS = _get_leapseconds()
 
 def _get_tai_offset_since(unix_time, leap_seconds=None):
     if leap_seconds is False:
